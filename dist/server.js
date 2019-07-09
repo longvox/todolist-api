e30d9b060677ecbc1ebe9b840a3213a4b56d2f37
imlong <imlong@pop-os.localdomain>
2019-07-09 14:28:19 +0700

    insert process port

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const PORT = 8088;
+const PORT = process.env.PORT || 8088;
 app_1.default.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 });
--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = 8088;
+const PORT = process.env.PORT || 8088;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
